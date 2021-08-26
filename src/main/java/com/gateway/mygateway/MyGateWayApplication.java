7234706ad79c88979f2b1b736fb72026e2cf4f83
枫枫 <fengfeng@fengfengdeMac-mini.local>
2021-08-26 23:46:54 +0800

    first commit

---
+++ src/main/java/com/gateway/mygateway/MyGateWayApplication.java
@@ -0,0 +1,13 @@
+package com.gateway.mygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGateWayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gateway/mygateway/MyGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.mygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGateWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
