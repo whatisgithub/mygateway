b0a5ab6b769e21d285a0543fc3e06e3438eb0857
woya2010@126.com <aERN8sYqyZN4r4a>
2021-08-28 12:14:33 +0800

    1

---
+++ src/main/java/com/gateway/mygateway/filter/MyFilter.java
@@ -0,0 +1,19 @@
+package com.gateway.mygateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class MyFilter implements Ordered, GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return null;
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
